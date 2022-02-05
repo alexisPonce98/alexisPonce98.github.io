fed30da7e4da42c8f9554e294c2bed2b22a3ee23
Alexis Ponce <ponce.alexis98@gmail.com>
2022-02-05 13:34:16 -0700

    fixed the positioning of the welcome sign

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+
+//function to enable animation for sidebar
 function openNav(){
    document.getElementById("sideNav").style.width = "250px";
 }
+//function to close the side bar
 function closeNav(){
     document.getElementById("sideNav").style.width = "0px"
 }
